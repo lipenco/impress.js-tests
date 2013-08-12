9b1985f1b00bde73697f4fe386e5d24ca7f43c76
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-12 13:27:45 +0400

    all 5 test in q-unit complited

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+    var translate = function ( t ) {
+        return " translate3d(" + t.x + "px," + t.y + "px," + t.z + "px) ";
+    };
+    
+    // `rotate` builds a rotate transform string for given data.
+    // By default the rotations are in X Y Z order that can be reverted by passing `true`
+    // as second parameter.
+    var rotate = function ( r, revert ) {
+        var rX = " rotateX(" + r.x + "deg) ",
+            rY = " rotateY(" + r.y + "deg) ",
+            rZ = " rotateZ(" + r.z + "deg) ";
+        
+        return revert ? rZ+rY+rX : rX+rY+rZ;
+    };
+    
+    // `scale` builds a scale transform string for given data.
+    var scale = function ( s ) {
+        return " scale(" + s + ") ";
+    };
+    
+    // `perspective` builds a perspective transform string for given data.
+    var perspective = function ( p ) {
+        return " perspective(" + p + "px) ";
+    };
+
+    var toNumber = function (numeric, fallback) {
+        return isNaN(numeric) ? (fallback || 0) : Number(numeric);
+    };
